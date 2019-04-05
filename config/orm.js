0b2aa72fa9292161ded9055775e8e470b74d5c6a
Bshields87 <shields.bethany@gmail.com>
2019-04-04 20:33:06 -0700

    initial push and beginning of directory creation

---
+++ config/orm.js
@@ -0,0 +1,24 @@
+var connection = require("../config/connection.js");
+
+selectAll()
+
+
+
+
+
+insertOne()
+
+
+
+
+updateOne()
+
+
+
+
+
+
+
+
+//export orm
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Fat Daddy", false);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("Cool Cat", false);
+
+INSERT INTO burger (burger_name, devoured)
+VALUES ("The Chu", false);
